2bbe5b8d9a1f3be70624d32a517900083b0682eb
tawhidmirza <102038583+tawhidmirza@users.noreply.github.com>
2022-07-02 21:08:35 +0600

    tawhid

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+if(!isset($_COOKIE['status'])){
+    header('location: login.html');
+}
+?>
+<html lang ="en">
+
+    <body background="1.jpg">
+&nbsp;&nbsp;&nbsp;
+    <h1>&nbsp;&nbsp;&nbsp;SELLER SITES <?php ?> <br><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="login.html">Log in</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="order.html">Confirm Order</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="addproduct.html">Add Product</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="editproduct.html">Edit Product Information</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="contactadmin.html">Contact Admin</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="closeselling.html">Close Selling</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="offer.html">Special Offers</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="payment.html">Payment</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="customer.html">Customers review</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="table.html">Booking Table</a><br>
+    &nbsp;&nbsp;&nbsp;
+    <a href="login.html">Logout</a><br>
+    </body>
+</html>
